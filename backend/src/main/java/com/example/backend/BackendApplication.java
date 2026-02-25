8db9536de52df1a2613eec8688d8cb8706cf1aa6
zmdsg <126111094+zmdsg@users.noreply.github.com>
2026-02-25 13:09:27 +0100

    test

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -3,7 +3,7 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//ein Kommentar
+//ein Kommentar mitz dem ich die Änderung in GitHub testen möchte
 
 @SpringBootApplication
 public class BackendApplication {
